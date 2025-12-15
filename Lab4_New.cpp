11db9f3fe30550ab3813a0c40ceeb458ab5a3a53
k3rrx <65369862+k3rrX@users.noreply.github.com>
2025-12-15 23:33:17 +0300

    Update Lab4_New.cpp

--- Lab4_New.cpp
+++ Lab4_New.cpp
@@ -4,322 +4,323 @@
 #include <cmath>
 #include <concepts>
 
-// Проверка чтоб тип был числовой
-template<typename TT>
-concept NumLike = std::is_scalar_v<TT>;
+using namespace std;
 
-#ifndef MY_PI
-#define MY_PI 3.14159265358979323846
+// проверка типа
+template<typename T>
+concept numtype = is_scalar_v<T>;
+
+#ifndef PI_VAL
+#define PI_VAL 3.14159265358979323846
 #endif
 
-// Точка с двумя координатами
-template<NumLike TT>
-class Spot {
-    TT x_val, y_val;
-public:
-    Spot(TT x = 0, TT y = 0) : x_val(x), y_val(y) {}
+// точка
+template<numtype T>
+struct tochka {
+    T a, b;
+    tochka(T x = 0, T y = 0) : a(x), b(y) {}
     
-    TT x() const { return x_val; }
-    TT y() const { return y_val; }
+    T geta() const { return a; }
+    T getb() const { return b; }
     
-    friend std::ostream& operator<<(std::ostream& os, const Spot& s) {
-        os << "[" << s.x_val << "," << s.y_val << "]";
-        return os;
+    friend ostream& operator<<(ostream& out, const tochka& t) {
+        out << "(" << t.a << "," << t.b << ")";
+        return out;
     }
     
-    bool check(const Spot& s2) const {
-        return x_val == s2.x_val && y_val == s2.y_val;
+    bool ravno(const tochka& t2) const {
+        return a == t2.a && b == t2.b;
     }
 };
 
-// Базовая фигура
-template<NumLike TT>
-class BaseFigura {
+// базовая фигура
+template<numtype T>
+class osnova {
 protected:
-    std::vector<std::unique_ptr<Spot<TT>>> dots;
+    vector<unique_ptr<tochka<T>>> list;
     
 public:
-    virtual ~BaseFigura() {}
+    virtual ~osnova() {}
     
-    virtual Spot<TT> center_point() const = 0;
-    virtual void show_dots(std::ostream& os) const = 0;
-    virtual void read_data(std::istream& is) = 0;
-    virtual double area_value() const = 0;
+    virtual tochka<T> seredina() const = 0;
+    virtual void pokazat(ostream& out) const = 0;
+    virtual void vvod(istream& in) = 0;
+    virtual double ploshad() const = 0;
     
-    friend std::ostream& operator<<(std::ostream& os, const BaseFigura& bf) {
-        bf.show_dots(os);
-        return os;
+    friend ostream& operator<<(ostream& out, const osnova& o) {
+        o.pokazat(out);
+        return out;
     }
     
-    friend std::istream& operator>>(std::istream& is, BaseFigura& bf) {
-        bf.read_data(is);
-        return is;
+    friend istream& operator>>(istream& in, osnova& o) {
+        o.vvod(in);
+        return in;
     }
     
-    virtual bool compare(const BaseFigura& bf2) const = 0;
+    virtual bool sravnit(const osnova& o2) const = 0;
     
-    operator double() const { return area_value(); }
+    operator double() const { return ploshad(); }
 };
 
-// Фигура с 5 углами
-template<NumLike TT>
-class Figura5 : public BaseFigura<TT> {
-    Spot<TT> cntr;
-    TT rad_val;
+// 5 угольник
+template<numtype T>
+class pyat : public osnova<T> {
+    tochka<T> centr;
+    T r;
     
-    void calc_dots() {
-        this->dots.clear();
-        for (int cnt = 0; cnt < 5; cnt++) {
-            double ang = 2.0 * MY_PI * cnt / 5.0;
-            TT x = cntr.x() + rad_val * std::cos(ang);
-            TT y = cntr.y() + rad_val * std::sin(ang);
-            this->dots.push_back(std::make_unique<Spot<TT>>(x, y));
+    void raschet() {
+        this->list.clear();
+        for (int i = 0; i < 5; i++) {
+            double ugol = 2.0 * PI_VAL * i / 5.0;
+            T x = centr.geta() + r * cos(ugol);
+            T y = centr.getb() + r * sin(ugol);
+            this->list.push_back(make_unique<tochka<T>>(x, y));
         }
     }
     
 public:
-    Figura5() : cntr(0, 0), rad_val(0) { calc_dots(); }
-    Figura5(TT cx, TT cy, TT r) : cntr(cx, cy), rad_val(r) { calc_dots(); }
+    pyat() : centr(0, 0), r(0) { raschet(); }
+    pyat(T cx, T cy, T rad) : centr(cx, cy), r(rad) { raschet(); }
     
-    Spot<TT> center_point() const override { return cntr; }
+    tochka<T> seredina() const override { return centr; }
     
-    void show_dots(std::ostream& os) const override {
-        os << "Figura5 dots: ";
-        for (const auto& d : this->dots) os << *d << " ";
+    void pokazat(ostream& out) const override {
+        out << "pyat tochki: ";
+        for (const auto& t : this->list) out << *t << " ";
     }
     
-    void read_data(std::istream& is) override {
-        TT a, b, c;
-        if (is >> a >> b >> c) {
-            cntr = Spot<TT>(a, b);
-            rad_val = c;
-            calc_dots();
+    void vvod(istream& in) override {
+        T x, y, rad;
+        if (in >> x >> y >> rad) {
+            centr = tochka<T>(x, y);
+            r = rad;
+            raschet();
         }
     }
     
-    double area_value() const override {
-        return 0.25 * std::sqrt(5.0 * (5.0 + 2.0 * std::sqrt(5.0))) * rad_val * rad_val;
+    double ploshad() const override {
+        return 0.25 * sqrt(5.0 * (5.0 + 2.0 * sqrt(5.0))) * r * r;
     }
     
-    bool compare(const BaseFigura<TT>& bf2) const override {
-        const Figura5* f = dynamic_cast<const Figura5*>(&bf2);
-        if (!f) return false;
-        return cntr.check(f->cntr) && rad_val == f->rad_val;
-    }
-};
-
-// Фигура с 6 углами
-template<NumLike TT>
-class Figura6 : public BaseFigura<TT> {
-    Spot<TT> cntr;
-    TT rad_val;
-    
-    void calc_dots() {
-        this->dots.clear();
-        for (int cnt = 0; cnt < 6; cnt++) {
-            double ang = 2.0 * MY_PI * cnt / 6.0;
-            TT x = cntr.x() + rad_val * std::cos(ang);
-            TT y = cntr.y() + rad_val * std::sin(ang);
-            this->dots.push_back(std::make_unique<Spot<TT>>(x, y));
-        }
-    }
-    
-public:
-    Figura6() : cntr(0, 0), rad_val(0) { calc_dots(); }
-    Figura6(TT cx, TT cy, TT r) : cntr(cx, cy), rad_val(r) { calc_dots(); }
-    
-    Spot<TT> center_point() const override { return cntr; }
-    
-    void show_dots(std::ostream& os) const override {
-        os << "Figura6 dots: ";
-        for (const auto& d : this->dots) os << *d << " ";
-    }
-    
-    void read_data(std::istream& is) override {
-        TT a, b, c;
-        if (is >> a >> b >> c) {
-            cntr = Spot<TT>(a, b);
-            rad_val = c;
-            calc_dots();
-        }
-    }
-    
-    double area_value() const override {
-        return 1.5 * std::sqrt(3.0) * rad_val * rad_val;
-    }
-    
-    bool compare(const BaseFigura<TT>& bf2) const override {
-        const Figura6* f = dynamic_cast<const Figura6*>(&bf2);
-        if (!f) return false;
-        return cntr.check(f->cntr) && rad_val == f->rad_val;
+    bool sravnit(const osnova<T>& o2) const override {
+        const pyat* p = dynamic_cast<const pyat*>(&o2);
+        if (!p) return false;
+        return centr.ravno(p->centr) && r == p->r;
     }
 };
 
-// Фигура с 8 углами
-template<NumLike TT>
-class Figura8 : public BaseFigura<TT> {
-    Spot<TT> cntr;
-    TT rad_val;
+// 6 угольник
+template<numtype T>
+class shest : public osnova<T> {
+    tochka<T> centr;
+    T r;
     
-    void calc_dots() {
-        this->dots.clear();
-        for (int cnt = 0; cnt < 8; cnt++) {
-            double ang = 2.0 * MY_PI * cnt / 8.0;
-            TT x = cntr.x() + rad_val * std::cos(ang);
-            TT y = cntr.y() + rad_val * std::sin(ang);
-            this->dots.push_back(std::make_unique<Spot<TT>>(x, y));
+    void raschet() {
+        this->list.clear();
+        for (int i = 0; i < 6; i++) {
+            double ugol = 2.0 * PI_VAL * i / 6.0;
+            T x = centr.geta() + r * cos(ugol);
+            T y = centr.getb() + r * sin(ugol);
+            this->list.push_back(make_unique<tochka<T>>(x, y));
         }
     }
     
 public:
-    Figura8() : cntr(0, 0), rad_val(0) { calc_dots(); }
-    Figura8(TT cx, TT cy, TT r) : cntr(cx, cy), rad_val(r) { calc_dots(); }
+    shest() : centr(0, 0), r(0) { raschet(); }
+    shest(T cx, T cy, T rad) : centr(cx, cy), r(rad) { raschet(); }
     
-    Spot<TT> center_point() const override { return cntr; }
+    tochka<T> seredina() const override { return centr; }
     
-    void show_dots(std::ostream& os) const override {
-        os << "Figura8 dots: ";
-        for (const auto& d : this->dots) os << *d << " ";
+    void pokazat(ostream& out) const override {
+        out << "shest tochki: ";
+        for (const auto& t : this->list) out << *t << " ";
     }
     
-    void read_data(std::istream& is) override {
-        TT a, b, c;
-        if (is >> a >> b >> c) {
-            cntr = Spot<TT>(a, b);
-            rad_val = c;
-            calc_dots();
+    void vvod(istream& in) override {
+        T x, y, rad;
+        if (in >> x >> y >> rad) {
+            centr = tochka<T>(x, y);
+            r = rad;
+            raschet();
         }
     }
     
-    double area_value() const override {
-        return 2.0 * std::sqrt(2.0) * rad_val * rad_val;
+    double ploshad() const override {
+        return 1.5 * sqrt(3.0) * r * r;
     }
     
-    bool compare(const BaseFigura<TT>& bf2) const override {
-        const Figura8* f = dynamic_cast<const Figura8*>(&bf2);
-        if (!f) return false;
-        return cntr.check(f->cntr) && rad_val == f->rad_val;
+    bool sravnit(const osnova<T>& o2) const override {
+        const shest* s = dynamic_cast<const shest*>(&o2);
+        if (!s) return false;
+        return centr.ravno(s->centr) && r == s->r;
     }
 };
 
-// Контейнер для хранения
-template<class ItemType>
-class Storage {
-    std::shared_ptr<ItemType[]> items;
-    size_t max_items;
-    size_t item_count;
+// 8 угольник
+template<numtype T>
+class vosem : public osnova<T> {
+    tochka<T> centr;
+    T r;
     
-    void resize_storage(size_t new_max) {
-        std::shared_ptr<ItemType[]> new_items(new ItemType[new_max]);
-        for (size_t i = 0; i < item_count; i++) {
-            new_items[i] = std::move(items[i]);
+    void raschet() {
+        this->list.clear();
+        for (int i = 0; i < 8; i++) {
+            double ugol = 2.0 * PI_VAL * i / 8.0;
+            T x = centr.geta() + r * cos(ugol);
+            T y = centr.getb() + r * sin(ugol);
+            this->list.push_back(make_unique<tochka<T>>(x, y));
         }
-        items = std::move(new_items);
-        max_items = new_max;
     }
     
 public:
-    Storage() : items(nullptr), max_items(0), item_count(0) {}
+    vosem() : centr(0, 0), r(0) { raschet(); }
+    vosem(T cx, T cy, T rad) : centr(cx, cy), r(rad) { raschet(); }
     
-    void add_item(const ItemType& item) {
-        if (item_count >= max_items) {
-            resize_storage(max_items == 0 ? 1 : max_items * 2);
+    tochka<T> seredina() const override { return centr; }
+    
+    void pokazat(ostream& out) const override {
+        out << "vosem tochki: ";
+        for (const auto& t : this->list) out << *t << " ";
+    }
+    
+    void vvod(istream& in) override {
+        T x, y, rad;
+        if (in >> x >> y >> rad) {
+            centr = tochka<T>(x, y);
+            r = rad;
+            raschet();
         }
-        items[item_count++] = item;
     }
     
-    void add_item(ItemType&& item) {
-        if (item_count >= max_items) {
-            resize_storage(max_items == 0 ? 1 : max_items * 2);
+    double ploshad() const override {
+        return 2.0 * sqrt(2.0) * r * r;
+    }
+    
+    bool sravnit(const osnova<T>& o2) const override {
+        const vosem* v = dynamic_cast<const vosem*>(&o2);
+        if (!v) return false;
+        return centr.ravno(v->centr) && r == v->r;
+    }
+};
+
+// массив
+template<class typ>
+class massiv {
+    shared_ptr<typ[]> data;
+    size_t cap;
+    size_t sz;
+    
+    void uvelichit(size_t newcap) {
+        shared_ptr<typ[]> newdata(new typ[newcap]);
+        for (size_t i = 0; i < sz; i++) {
+            newdata[i] = move(data[i]);
         }
-        items[item_count] = std::move(item);
-        item_count++;
+        data = move(newdata);
+        cap = newcap;
     }
     
-    void remove_item(size_t pos) {
-        if (pos >= item_count) return;
-        for (size_t i = pos; i < item_count - 1; i++) {
-            items[i] = std::move(items[i + 1]);
+public:
+    massiv() : data(nullptr), cap(0), sz(0) {}
+    
+    void dobavit(const typ& item) {
+        if (sz >= cap) {
+            uvelichit(cap == 0 ? 1 : cap * 2);
         }
-        item_count--;
+        data[sz++] = item;
     }
     
-    ItemType& get_item(size_t pos) {
-        return items[pos];
+    void dobavit(typ&& item) {
+        if (sz >= cap) {
+            uvelichit(cap == 0 ? 1 : cap * 2);
+        }
+        data[sz] = move(item);
+        sz++;
     }
     
-    const ItemType& get_item(size_t pos) const {
-        return items[pos];
+    void udalit(size_t pos) {
+        if (pos >= sz) return;
+        for (size_t i = pos; i < sz - 1; i++) {
+            data[i] = move(data[i + 1]);
+        }
+        sz--;
     }
     
-    size_t size() const { return item_count; }
+    typ& operator[](size_t pos) {
+        return data[pos];
+    }
     
-    double total_area() const {
+    const typ& operator[](size_t pos) const {
+        return data[pos];
+    }
+    
+    size_t razmer() const { return sz; }
+    
+    double vseploshad() const {
         double sum = 0;
-        for (size_t i = 0; i < item_count; i++) {
-            sum += static_cast<double>(*items[i]);
+        for (size_t i = 0; i < sz; i++) {
+            sum += static_cast<double>(*data[i]);
         }
         return sum;
     }
     
-    void display_all() const {
-        for (size_t i = 0; i < item_count; i++) {
-            std::cout << "Item " << i + 1 << ":\n";
-            std::cout << "  " << *items[i] << "\n";
-            std::cout << "  Center: " << items[i]->center_point() << "\n";
-            std::cout << "  Area: " << static_cast<double>(*items[i]) << "\n\n";
+    void pokazatvse() const {
+        for (size_t i = 0; i < sz; i++) {
+            cout << "Figura " << i + 1 << ":\n";
+            cout << "  " << *data[i] << "\n";
+            cout << "  Seredina: " << data[i]->seredina() << "\n";
+            cout << "  Ploshad: " << static_cast<double>(*data[i]) << "\n\n";
         }
     }
 };
 
 int main() {
-    std::cout << "Program start\n";
-    std::cout << "Working with shapes\n\n";
+    cout << "Start programmi\n";
+    cout << "Rabota s figurami\n\n";
     
-    // Часть 1
-    std::cout << "Part 1: Storage test\n";
-    Storage<std::shared_ptr<BaseFigura<double>>> store1;
+    // test 1
+    cout << "Test 1: massiv figur\n";
+    massiv<shared_ptr<osnova<double>>> m1;
     
-    store1.add_item(std::make_shared<Figura5<double>>(0, 0, 5.0));
-    store1.add_item(std::make_shared<Figura6<double>>(2, 2, 3.0));
-    store1.add_item(std::make_shared<Figura8<double>>(-1, -1, 4.0));
+    m1.dobavit(make_shared<pyat<double>>(0, 0, 5.0));
+    m1.dobavit(make_shared<shest<double>>(2, 2, 3.0));
+    m1.dobavit(make_shared<vosem<double>>(-1, -1, 4.0));
     
-    store1.display_all();
-    std::cout << "Total area: " << store1.total_area() << "\n\n";
+    m1.pokazatvse();
+    cout << "Obshaya ploshad: " << m1.vseploshad() << "\n\n";
     
-    store1.remove_item(1);
-    std::cout << "After remove:\n";
-    store1.display_all();
+    m1.udalit(1);
+    cout << "Posle udaleniya:\n";
+    m1.pokazatvse();
     
-    // Часть 2
-    std::cout << "\nPart 2: Figura6 with int\n";
-    Figura6<int> f6(0, 0, 10);
-    std::cout << "Area: " << static_cast<double>(f6) << "\n";
-    std::cout << "Center: " << f6.center_point() << "\n";
-    f6.show_dots(std::cout);
-    std::cout << "\n";
+    // test 2
+    cout << "\nTest 2: shest s int\n";
+    shest<int> s1(0, 0, 10);
+    cout << "Ploshad: " << static_cast<double>(s1) << "\n";
+    cout << "Seredina: " << s1.seredina() << "\n";
+    s1.pokazat(cout);
+    cout << "\n";
     
-    // Часть 3
-    std::cout << "\nPart 3: Input Figura5\n";
-    std::cout << "Enter 3 numbers: ";
-    Figura5<double> f5_input;
-    std::cin >> f5_input;
-    std::cout << "Area: " << static_cast<double>(f5_input) << "\n";
-    std::cout << "Center: " << f5_input.center_point() << "\n";
+    // test 3
+    cout << "\nTest 3: Vvod pyat\n";
+    cout << "Vvedite 3 chisla: ";
+    pyat<double> p1;
+    cin >> p1;
+    cout << "Ploshad: " << static_cast<double>(p1) << "\n";
+    cout << "Seredina: " << p1.seredina() << "\n";
     
-    // Часть 4
-    std::cout << "\nPart 4: Float shapes\n";
-    Storage<std::shared_ptr<Figura6<float>>> store2;
-    store2.add_item(std::make_shared<Figura6<float>>(0.0f, 0.0f, 2.0f));
-    store2.add_item(std::make_shared<Figura6<float>>(1.0f, 1.0f, 1.5f));
+    // test 4
+    cout << "\nTest 4: Float figuri\n";
+    massiv<shared_ptr<shest<float>>> m2;
+    m2.dobavit(make_shared<shest<float>>(0.0f, 0.0f, 2.0f));
+    m2.dobavit(make_shared<shest<float>>(1.0f, 1.0f, 1.5f));
     
-    for (size_t i = 0; i < store2.size(); i++) {
-        std::cout << "Shape " << i + 1 << " area: " 
-                  << static_cast<double>(*store2.get_item(i)) << "\n";
+    for (size_t i = 0; i < m2.razmer(); i++) {
+        cout << "Figura " << i + 1 << " ploshad: " 
+             << static_cast<double>(*m2[i]) << "\n";
     }
     
-    std::cout << "\nPress Enter to end...";
-    std::cin.ignore();
-    std::cin.get();
+    cout << "\nPress Enter...";
+    cin.ignore();
+    cin.get();
     return 0;
 }
